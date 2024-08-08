9b8654a86923ad2dad5cde23b35ba8550e8c0e88
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-08-07 23:19:06 -0300

    feat: add login verification to see if the user is logged in and display the login and create account routes if its not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Header } from "@/components/ui/header";
-import { useState } from "react";
-import { getCookie } from "cookies-next";
+import { useEffect, useState } from "react";
+import { CookieValueTypes, getCookie } from "cookies-next";
 import { toast, Toaster } from "sonner";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import {
@@ -19,8 +19,13 @@ export default function Home() {
   const [link, setLink] = useState("");
   const [error, setError] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [loggedIn, setLoggedIn] = useState<CookieValueTypes>("");
 
-  const loggedIn = getCookie("token");
+  useEffect(() => {
+    setLoggedIn(getCookie("token"));
+  }, []);
+
+  const isLoggedIn = loggedIn ? true : false;
 
   function handleOpenDialog(event: React.FormEvent<HTMLFormElement>) {
     setError(false);
@@ -69,7 +74,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <Header />
+      <Header isLoggedIn={isLoggedIn} />
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
