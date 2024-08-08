1767b97e36a451b80ed84131f9985a7b51e6b6f0
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-08-07 22:10:45 -0300

    feat: add login verification to see if the user is logged in and display the login and create account routes if its not

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,33 +1,46 @@
 import { LinkIcon } from "lucide-react";
 import Link from "next/link";
 
-export function Header() {
+export function Header({ isLoggedIn }: { isLoggedIn: boolean }) {
+  console.log(isLoggedIn);
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
       <Link
-        href="#"
+        href="/"
         className="flex items-center justify-center"
         prefetch={false}
       >
         <LinkIcon className="h-6 w-6" />
         <span className="sr-only">URL Shortener</span>
       </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link
-          href="login"
-          className="text-sm font-medium hover:underline underline-offset-4"
-          prefetch={false}
-        >
-          Login
-        </Link>
-        <Link
-          href="create-account"
-          className="text-sm font-medium hover:underline underline-offset-4"
-          prefetch={false}
-        >
-          Create Account
-        </Link>
-      </nav>
+      {!isLoggedIn ? (
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link
+            href="login"
+            className="text-sm font-medium hover:underline underline-offset-4"
+            prefetch={false}
+          >
+            Login
+          </Link>
+          <Link
+            href="create-account"
+            className="text-sm font-medium hover:underline underline-offset-4"
+            prefetch={false}
+          >
+            Create Account
+          </Link>
+        </nav>
+      ) : (
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link
+            href="dashboard"
+            className="text-sm font-medium hover:underline underline-offset-4"
+            prefetch={false}
+          >
+            Dashboard
+          </Link>
+        </nav>
+      )}
     </header>
   );
 }
